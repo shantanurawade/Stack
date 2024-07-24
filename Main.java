5730c015023394bdef5453761fba5c72358a2899
shantanurawade <103266755+shantanurawade@users.noreply.github.com>
2024-07-24 10:25:20 +0530

    Add files via upload
    Main file.

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main{
+
+    public static void main(String[] args){
+
+        // Creating object for Init class to call init method
+        InitStack init = new InitStack();
+        init.init();
+    }
+}
+
